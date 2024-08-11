2f9091ee82b9d4b470d932122fc140a355c22ec8
Igor <148789951+Donar31@users.noreply.github.com>
2024-08-11 12:48:46 +0300

    11.08

---
+++ js/util.js
@@ -0,0 +1,14 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+
+const counter = () => {
+  let n = 0;
+  return () => ++n;
+};
+
+export { getRandomInteger, counter };
