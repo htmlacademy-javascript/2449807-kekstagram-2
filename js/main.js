2a88a0f38480c5ea018d114d3c934559c7865e0f
Igor <148789951+Donar31@users.noreply.github.com>
2024-09-30 13:24:58 +0300

    30.09

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 import { getData } from './api.js';
-import { renderCards } from './render-card.js';
+
 import { showAlert } from './util.js';
 import './upload-form.js';
+import { init as initFilter } from './filter.js';
 
 getData()
   .then((data) => {
-    renderCards(data);
+    initFilter(data);
   })
   .catch((message) => {
     showAlert(`Что-то пошло не так... ${message}`);
